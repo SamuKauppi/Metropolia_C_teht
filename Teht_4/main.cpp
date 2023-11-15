f4a00f59bcc503ac3c702e998f7b5e064db44785
HP <samu.kauppi@metropolia.fi>
2023-11-15 19:55:06 +0200

    add teht 4

---
+++ Teht_4/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class Auto 
+{
+private:
+	string _rekisteri;
+	double _huippuNopeus;
+	double _nopeus;
+	double _matka;
+
+public:
+	Auto(string rekisteri, double huippuNopeus)
+	{
+		_rekisteri = rekisteri;
+		_huippuNopeus = huippuNopeus;
+		_nopeus = 0;
+		_matka = 0;
+	}
+	void kiihduta(double nopeudenMuutos)
+	{
+		_nopeus += nopeudenMuutos;
+		_nopeus = fmax(0, fmin(_nopeus, _huippuNopeus));
+	}
+	void kulje(double aika)
+	{
+		_matka += _nopeus * aika;
+	}
+
+	double palautaNopeus()
+	{
+		return _nopeus;
+	}
+
+	string palautaRekisteri()
+	{
+		return _rekisteri;
+	}
+
+	double palautaMatka()
+	{
+		return _matka;
+	}
+};
+
+string riviVaiht()
+{
+	return "\n";
+}
+
+void kokeilu()
+{
+	Auto formula = Auto("ABC-123", 142);
+	cout << "rekisteri: " << formula.palautaRekisteri() << riviVaiht();
+	formula.kiihduta(30);
+	formula.kiihduta(70);
+	formula.kiihduta(50);
+	cout << formula.palautaNopeus() << riviVaiht();
+	formula.kiihduta(-200);
+	cout << formula.palautaNopeus() << riviVaiht();
+}
+
+int DetermineRandomValue(int offset, int range)
+{
+	srand((unsigned)time(NULL));
+	int random = offset + (rand() % range + 1);
+	return random;
+}
+
+int main()
+{
+	kokeilu();
+	string nimi = "ABC-";
+	list<Auto> kilpailijat{};
+	for (int i = 0; i < 10; i++)
+	{
+		double satunnainenNopeus = DetermineRandomValue(100, 200);
+		Auto tempAuto = Auto(nimi + to_string(i + 1), satunnainenNopeus);
+		kilpailijat.push_back(tempAuto);
+	}
+
+	Auto* voittaja = 0;
+	while (!voittaja)
+	{
+		for (auto& i : kilpailijat)
+		{
+			double satunnainenNopeus = DetermineRandomValue(-10, 15);
+			i.kiihduta(satunnainenNopeus);
+			i.kulje(1);
+
+			if (i.palautaMatka() >= 1000)
+			{
+				voittaja = &i;
+				break;
+			}
+		}
+	}
+
+	cout << "Voittaja loytyi!" << riviVaiht();
+	for (auto i : kilpailijat)
+	{
+		cout << "Kilpailija " << i.palautaRekisteri() << " kulki " << i.palautaMatka() << riviVaiht();
+	}
+	cout << riviVaiht() << "Voittaja on: " << voittaja->palautaRekisteri();
+
+	return 0;
+}
